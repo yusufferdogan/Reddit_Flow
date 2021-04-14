03143e9d668e005ecd7a9c792aa6be2cbdfabafb
yusuf erdogan <a.erdogan2018@gtu.edu.tr>
2021-04-14 03:05:37 +0300

    prints deleted

--- lib/services/post_service.dart
+++ lib/services/post_service.dart
@@ -13,30 +13,24 @@ class PostService {
   }
 
   Future<List<Post>> getAllPosts() async {
-    print("getAllPosts");
     var url = "https://www.reddit.com/r/flutterdev/top.json?count=20";
     final List<Post> loadedPosts = [];
     try {
       final response = await http.get(url);
       if (response.statusCode == 200) {
-        print('? ${response.statusCode}');
         var extractedData = jsonDecode(response.body) as Map<String, dynamic>;
         final x = extractedData['data']['children'] as List<dynamic>;
-        print('len: ${x.length}');
         for (int i = 0; i < x.length; i++) {
-          print(x[i]['data']['selftext']);
-          print(i);
           final post = Post.fromJson(
               i.toString(), x[i]['data'], x[i]['data']['thumbnail']);
           loadedPosts.add(post);
         }
-        for (int i = 0; i < x.length; i++) {
-          print(loadedPosts[i].toString());
-        }
       } else {
         print('error ${response.statusCode}');
       }
-    } catch (e) {} finally {}
+    } catch (e) {
+      print(e);
+    } finally {}
     return loadedPosts;
   }
 }
